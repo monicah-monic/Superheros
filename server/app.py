77ee5d4843ab8b8ee5a239d78472e91fea6c0880
monicah-monic <monicahwanjiru205@gmail.com>
2025-06-16 20:45:27 +0300

    psot heropoower

--- server/app.py
+++ server/app.py
@@ -26,29 +26,6 @@ def get_heros():
         for hero in heros
         ])
 
-# @app.route('/heros/<int:id>', methods=['GET'])
-# def get_heros_by_id(id):
-#     hero = Hero.query.get(id)
-#     try:
-#        if hero:
-#         return jsonify([{
-#              "id": hero.id,
-#             "name": hero.name,
-#             "super_name": hero.super_name,
-#             "hero_powers":[{
-#                 "hero_id":hero.id,
-#                 "id": hero.id,
-#                 "description": hero.descrption
-
-#             }
-#                            ]
-#         }])
-#     except Exception as e:
-#         print(f"Power not found: {e}")
-#         return make_response(jsonify({"error": "Internal server error"}), 500)
-    
-
-
 @app.route('/powers', methods=['GET'])
 def get_powers():
     powers= Power.query.all()
@@ -101,14 +78,62 @@ def update_power_by_id(id):
         return make_response(jsonify({"error": "Internal server error"}), 500)
     
 
-   
+@app.route('/hero_powers', methods=['POST'])
+def add_hero_power():
+    data = request.get_json()
+ 
+    if 'strength' not in data:
+        return jsonify({'error': ['Strength is required']}), 400
 
+    hero = Hero.query.get(data['hero_id'])
+    power = Power.query.get(data['power_id'])
 
+    if not hero:
+        return jsonify({'error': 'Hero not found'}), 404
+    
+    if not power:
+        return jsonify({'error': 'Power not found'}), 404
 
+    try:
+        hero_power = HeroPower(
+            strength=data["strength"],
+            hero_id=data["hero_id"],
+            power_id=data["power_id"]
+        )
+        db.session.add(hero_power)
+        db.session.commit()
 
+        hero_power_details = hero_power.to_dict(only=('id', 'hero_id', 'power_id', 'strength'))
+        hero_power_details['hero'] = hero_power.hero.to_dict(only=('id', 'name', 'super_name'))
+        hero_power_details['power'] = hero_power.power.to_dict(only=('id', 'name', 'description'))
 
+        return jsonify(hero_power_details), 201
 
+    except ValueError as exc:
+        db.session.rollback()
+        return jsonify({'errors': str(exc)}), 400   
+    
 
+# @app.route('/heros/<int:id>', methods=['GET'])
+# def get_heros_by_id(id):
+#     hero = Hero.query.get(id)
+#     try:
+#        if hero:
+#         return jsonify([{
+#              "id": hero.id,
+#             "name": hero.name,
+#             "super_name": hero.super_name,
+#             "hero_powers":[{
+#                 "hero_id":hero.id,
+#                 "id": hero.id,
+#                 "description": hero.descrption
+
+#             }
+#                            ]
+#         }])
+#     except Exception as e:
+#         print(f"Power not found: {e}")
+#         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
