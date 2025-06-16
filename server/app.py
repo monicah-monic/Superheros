0ec3f80c2aa3cb346077158288c16049b13129fa
monicah-monic <monicahwanjiru205@gmail.com>
2025-06-16 20:44:19 +0300

    post heropower

--- server/app.py
+++ server/app.py
@@ -26,9 +26,27 @@ def get_heros():
         for hero in heros
         ])
 
+# @app.route('/heros/<int:id>', methods=['GET'])
+# def get_heros_by_id(id):
+#     hero = Hero.query.get(id)
+#     try:
+#        if hero:
+#         return jsonify([{
+#              "id": hero.id,
+#             "name": hero.name,
+#             "super_name": hero.super_name,
+#             "hero_powers":[{
+#                 "hero_id":hero.id,
+#                 "id": hero.id,
+#                 "description": hero.descrption
 
-
-
+#             }
+#                            ]
+#         }])
+#     except Exception as e:
+#         print(f"Power not found: {e}")
+#         return make_response(jsonify({"error": "Internal server error"}), 500)
+    
 
 
 @app.route('/powers', methods=['GET'])
@@ -57,15 +75,32 @@ def get_powers_by_id(id):
         ])
     except Exception as e:
         print(f"Power not found: {e}")
+        return jsonify({"error": "Internal server error"}), 500
+    
+@app.route('/powers/<int:id>', methods=['PATCH'])    
+def update_power_by_id(id):
+    try:
+        power = Power.query.get(id)
+        if not power:
+            return jsonify({"error": "Power not found"}), 404
+
+        data = request.get_json()
+        new_description = data.get('description')
+        if new_description:
+            power.description = new_description
+            db.session.commit()
+
+        return jsonify({
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        })
+
+    except Exception as e:
+        print(f"Error updating power: {e}")
         return make_response(jsonify({"error": "Internal server error"}), 500)
     
-    
-# @app.route('/heros/<int:id>', methods=['GET'])
-# def get_heros_by_id(id):
-#     hero = Hero.query.get(id)
-#     try:
-#        if hero:
-#         return jsonify
+
    
 
 
